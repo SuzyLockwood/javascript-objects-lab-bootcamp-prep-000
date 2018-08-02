0e09d2051d57a860fe1af7e81b2604188b4f221a
Susan Lockwood <susan.m.mullin@gmail.com>
2018-08-02 04:43:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-var recipes = {}
+var recipes = {
+  cake: 'sugar, butter, eggs, vanilla extract, flour, baking powder, milk',
+  smoothie: 'fruit, milk, ice'
+} 
  function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, { [key]: value })
-}
+  return Object.assign({}, object, {[key]: value})
+} 
  function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object;
-}
+ object[key] = value
+ return object
+} 
  function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object);
-  delete newObj.key;
-  return newObj;
-}
+  var newRecipe = Object.assign({}, object)
+  newRecipe // { foo: 'bar' }
+  delete newRecipe.key // true
+  return newRecipe
+} 
  function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key;
-  return object;
-}
+  delete object.key
+  return object
+} 
